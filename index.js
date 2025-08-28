f609e1634ea442b6462e5556c9085daac57feb30
tharakeshrajesh <tharakeshrajesh3@gmail.com>
2025-08-27 19:40:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-process.env.FFMPEG_PATH = require('ffmpeg-static');
 require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
-const { Client, GatewayIntentBits, Events, messageLink, Guild } = require('discord.js');
+const { Client, GatewayIntentBits, Events, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require('discord.js');
 const {
   joinVoiceChannel,
   createAudioPlayer,
   createAudioResource,
   AudioPlayerStatus,
 } = require('@discordjs/voice');
+const { start } = require('repl');
+const { exitCode } = require('process');
+const { clear } = require('console');
 
 const client = new Client({
   intents: [
@@ -28,7 +30,18 @@ const inVC = new Map();
 const connection = new Map();
 const lastUsed = new Map();
 const inactivityCall = new Map();
+const messages = new Map();
 let globalLastUsed = 0;
+
+async function clearValues(key) {
+  if (!messages.has(key)) return;
+  const m = messages.get(key);
+  const row = new ActionRowBuilder().addComponents(
+    ButtonBuilder.from(m.components[0].components[0]).setDisabled(true)
+  );
+  await m.edit({ components: [row] });
+  messages.delete(key);
+}
 
 client.once(Events.ClientReady, () => {
   console.log(`✅ Logged in as ${client.user.tag}`);
@@ -39,27 +52,12 @@ client.on('messageCreate', async (message) => {
 
   const guildId = message.guild.id;
 
-  if (message.content.toLowerCase() === "*join") {
-    if (inVC.get(guildId)) return message.reply('❗ I am already in a voice channel!');
-
-    if (!message.member.voice.channel) return message.reply('❗ You must join a voice channel first!');
-
-    joinVoiceChannel({
-      channelId: message.member.voice.channel.id,
-      guildId: message.guildId,
-      adapterCreator: message.guild.voiceAdapterCreator,
-    });
-
-    inVC.set(guildId, true);
-    lastUsed.set(guildId, Date.now());
-    message.channel.send('🏁 Get ready to type! Run *startrace to start.');
-  } else if (message.content.toLowerCase() === "*leave") {
-      if (!inVC.get(guildId)) return message.reply('❗ I am not in a voice channel!');
-      if (gameActive.get(guildId)) return message.reply('❗ There is an ongoing race!');
-      leaveVC(message);
-  } else if (message.content.toLowerCase() === '*startrace' && !gameActive.get(guildId)) {
+  async function startGame() {
+    if (!gameActive.get(guildId)) {
       if (!message.member.voice.channel || !inVC.get(guildId))
         return message.reply('❗ Join a voice channel first then run *join.');
+
+      if (messages.has(guildId)) messages.delete(guildId);
 
       const conn = joinVoiceChannel({
         channelId: message.member.voice.channel.id,
@@ -87,8 +85,16 @@ client.on('messageCreate', async (message) => {
           player.play(resource);
           playingaudio.set(guildId, true);
 
-          player.on(AudioPlayerStatus.Idle, () => {
-            message.channel.send('⌨️ Type the sentence you just heard — first to do it exactly wins!');
+          player.on(AudioPlayerStatus.Idle, async () => {
+            const row = new ActionRowBuilder()
+              .addComponents(
+                new ButtonBuilder()
+                  .setCustomId('repeat')
+                  .setLabel('Repeat')
+                  .setStyle(ButtonStyle.Primary)
+              );
+            const m = await message.channel.send({ content: '⌨️ Type the sentence you just heard — first to do it exactly wins!' , components: [row] });
+            messages.set(guildId, m);
             playingaudio.set(guildId, false);
             startTime.set(guildId, Date.now());
           });
@@ -100,15 +106,19 @@ client.on('messageCreate', async (message) => {
 
       lastUsed.set(guildId, Date.now());
       globalLastUsed = Date.now();
-  } else if (message.content.toLowerCase() === "*help") {
-      console.log('help');
-  } else if (
-      message.content.toLowerCase() === '*repeat' &&
+    }
+  }
+
+  function repeatAudio(member) {
+    if (
       gameActive.get(guildId) &&
       inVC.get(guildId) &&
       !playingaudio.get(guildId) &&
-      connection.get(guildId)
-  ) {
+      connection.get(guildId) &&
+      member.voice.channel &&
+      member.guild.members.me.voice.channel &&
+      member.voice.channel.id === member.guild.members.me.voice.channel.id
+    ) {
       try {
         const player = createAudioPlayer();
         const resource = createAudioResource(path.join(__dirname, `${guildId}.mp3`));
@@ -120,9 +130,37 @@ client.on('messageCreate', async (message) => {
         console.error('TTS Error:', err);
         message.channel.send('⚠️ Failed to repeat audio.');
       }
+    }
+  }
+
+  if (message.content.toLowerCase() === "*join") {
+    if (inVC.get(guildId)) return message.reply('❗ I am already in a voice channel!');
+
+    if (!message.member.voice.channel) return message.reply('❗ You must join a voice channel first!');
+
+    const conn = joinVoiceChannel({
+      channelId: message.member.voice.channel.id,
+      guildId: message.guildId,
+      adapterCreator: message.guild.voiceAdapterCreator,
+    });
+
+    connection.set(guildId, conn);
+    inVC.set(guildId, true);
+    lastUsed.set(guildId, Date.now());
+    const row = new ActionRowBuilder()
+    .addComponents(
+      new ButtonBuilder()
+        .setCustomId('start')
+        .setLabel('Start game')
+        .setStyle(ButtonStyle.Primary)
+    );
+    const m = await message.channel.send({ content: '🏁 Get ready to type! Click the button below to start.', components: [row] });
+    messages.set(guildId, m);
+  } else if (message.content.toLowerCase() === "*help") {
+      console.log('help');
   } else if (
     gameActive.get(guildId) &&
-    message.content.trim().toLowerCase() === 
+    message.content.trim().toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()'"?<>@\[\]\\|+]/g, '') === 
     currentSentence.get(guildId)?.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()'"?<>@\[\]\\|+]/g, '')) {
 
       gameActive.set(guildId, false);
@@ -130,9 +168,23 @@ client.on('messageCreate', async (message) => {
       const endTime = Date.now();
       const timeTaken = ((endTime - startTime.get(guildId)) / 1000).toFixed(2);
       const ttsUrl = `https://api.streamelements.com/kappa/v2/speech?voice=Brian&text=Congrats ${message.author.displayName}, you won with a time of ${timeTaken} seconds!`;
-      message.channel.send(`🏆 ${message.author.displayName} wins the typing race with a time of ${timeTaken} seconds!`);
+      const row = new ActionRowBuilder()
+      .addComponents(
+        new ButtonBuilder()
+          .setCustomId('start')
+          .setLabel('Start game')
+          .setStyle(ButtonStyle.Primary)
+      );
+      row.addComponents(
+        new ButtonBuilder()
+          .setCustomId('leave')
+          .setLabel('Leave')
+          .setStyle(ButtonStyle.Danger)
+      );
+      message.channel.send({ content: `🏆 ${message.author.displayName} wins the typing race with a time of ${timeTaken} seconds!`, components: [row]});
       
       try {
+        clearValues(guildId);
         const response = await axios.get(ttsUrl, { responseType: 'stream' });
         const writer = fs.createWriteStream(path.join(__dirname, `${guildId}.mp3`));
         response.data.pipe(writer);
@@ -161,19 +213,47 @@ client.on('messageCreate', async (message) => {
   }
 
   inactivityCall.set(guildId, setInterval(inactivity, 180000));
+
+  client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isButton()) return;
+
+    await interaction.deferReply({ flags: 64 }).catch(() => {});
+
+    try {
+      if (interaction.customId === 'repeat') {
+        await repeatAudio(interaction.member);
+        await interaction.editReply({ content: "Repeating audio..." });
+      } else if (interaction.customId === 'start') {
+        const row = new ActionRowBuilder().addComponents(
+          ButtonBuilder.from(interaction.message.components[0].components[0]).setDisabled(true)
+        );
+        await interaction.message.edit({ components: [row] });
+        await startGame();
+        await interaction.editReply({ content: "Game started" });
+      } else if (interaction.customId === 'leave') {
+        await leaveVC(message);
+        await interaction.editReply({ content: "Left the voice channel" });
+        const row = new ActionRowBuilder().addComponents(
+          ButtonBuilder.from(interaction.message.components[0].components[0]).setDisabled(true)
+        );
+        await interaction.message.edit({ components: [row] });
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  });
 });
 
 function leaveVC(message, v=false) {
   const guildId = message.guild.id;
 
+  if (!inVC.get(guildId)) return message.reply('❗ I am not in a voice channel!');
+    if (gameActive.get(guildId)) return message.reply('❗ There is an ongoing race!');
+
   try {
-    const conn = joinVoiceChannel({
-        channelId: message.member.voice.channel.id,
-        guildId: guildId,
-        adapterCreator: message.guild.voiceAdapterCreator,
-      });
-    if (conn)
-    conn.destroy();
+    
+    const conn = connection.get(guildId);
+    if (conn) conn.destroy();
 
     connection.delete(guildId);
     inVC.delete(guildId);
